2def664ff90f96041c53288986155be3e5c6b549
Prace Fen <fenprace@gmail.com>
2016-09-21 10:22:53 +0800

    Readme.md written

---
+++ patch-of-bitjwc.user.js
@@ -0,0 +1,30 @@
+// Patch of BITJWC User Script
+// version 1.0!
+// Copyright (c) 2016, Prace Fen
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+//
+// ==UserScript==
+// @name          Patch of BITJWC
+// @description   A script fixing bugs on the pages of jwc.bit.edu.cn.
+// @include       http://10.5.2.80/*
+// ==/UserScript==
+
+var replacer = document.createElement("script");
+
+replacer.innerHTML = "\
+function hidemenu(main, menuid){\
+  var element = document.getElementById(menuid);\
+  element.style.cssText = 'visibility: hidden;';\
+}\
+\
+function showmenu(main, menuid) {\
+  var switcher = document.getElementById(main);\
+  var element = document.getElementById(menuid);\
+  element.style.cssText =\
+    'visibility: visible; top: 70px; left: '\
+    + switcher.offsetLeft\
+    + 'px;';\
+}"
+
+document.body.insertBefore(replacer, document.body.lastChild);
